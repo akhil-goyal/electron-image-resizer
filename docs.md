97740ac76a030575051f1d44bccf9c7571b18608
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-28 22:01:41 -0400

    Added file loader & icon for the application.

---
+++ docs.md
@@ -0,0 +1,14 @@
+# DOCS
+
+loadURL method helps to load a URL when the application is launched.
+
+```
+mainWindow.loadURL(`https://example.com`);
+```
+
+loadFile method also helps to load a URL when the application is launched, it doesn't 
+require defining of protocols.
+
+```
+    mainWindow.loadFile('./app/index.html');
+```
--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 // browserWindow is used to create desktop window.
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow
+
 const createMainWindow = () => {
 
     // Desktop Window configuration
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'ShrinkIT',
         width: 500,
-        height: 600
+        height: 600,
+        icon: './assets/icons/Icon_256x256.png'
     });
+
+    mainWindow.loadFile('./app/index.html');
 
 }
 
