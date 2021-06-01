e5321e89b0130a283c25b8702b8355098eba88f7
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-06-01 16:42:41 -0400

    Created Application Shortcuts.

--- main.js
+++ main.js
@@ -47,16 +47,30 @@ app.on('ready', () => {
 const menu = [
     ...(isMac ? [{ role: 'appMenu' }] : []),
     {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
-                accelerator: 'CmdOrCtrl+W',
-                click: () => app.quit()
-            }
-        ]
-    }
+        role: 'fileMenu'
+        // label: 'File',
+        // submenu: [
+        //     {
+        //         label: 'Quit',
+        //         // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+        //         accelerator: 'CmdOrCtrl+W',
+        //         click: () => app.quit()
+        //     }
+        // ]
+    },
+    ...(isDev ? [
+        {
+            label: 'Developer',
+            submenu: [
+                {
+                    role: 'reload',
+                    role: 'forcereload',
+                    type: 'separator',
+                    role: 'toggledevtools',
+                }
+            ]
+        }
+    ] : [])
 ];
 
 app.on('window-all-closed', () => {
